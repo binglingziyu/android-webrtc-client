21f8124c5999f1ceadc1488c6c7461b3995b7f36
hubin <hubin.android@gmail.com>
2020-11-12 18:35:13 +0800

    登录页，需要填写一个用户名；

--- app/src/main/java/com/ihubin/webrtc/page/LoginActivity.kt
+++ app/src/main/java/com/ihubin/webrtc/page/LoginActivity.kt
@@ -3,19 +3,29 @@ package com.ihubin.webrtc.page
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.ihubin.webrtc.R
 
 class LoginActivity : AppCompatActivity() {
+
+    private var nameInput: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         title = "登录"
         setContentView(R.layout.activity_login)
 
+        nameInput = findViewById(R.id.name_input)
+
     }
 
     fun login(view: View) {
+        if(nameInput?.text == null || nameInput?.text?.length == 0) {
+            Toast.makeText(this, "填写用户名", Toast.LENGTH_SHORT).show()
+            return
+        }
         startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
